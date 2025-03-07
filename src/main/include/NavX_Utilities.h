e2f171353e40cf651a2251d6716184f47b3e47a3
Commandrew1337 <apanning@gmail.com>
2025-03-07 10:18:10 -0600

    comment added to navx method

--- src/main/include/NavX_Utilities.h
+++ src/main/include/NavX_Utilities.h
@@ -1,7 +1,12 @@
 #pragma once
 
 enum PinType { DigitalIO, PWM, AnalogIn, AnalogOut };
+
+/* GetChannelFromPin( PinType, int ) - converts from a navX-MXP */
+/* Pin type and number to the corresponding RoboRIO Channel     */
+/* Number, which is used by the WPI Library functions.          */
 int GetChannelFromPin( PinType type, int io_pin_number );
+
 static const int MAX_NAVX_MXP_DIGIO_PIN_NUMBER      = 9;
 static const int MAX_NAVX_MXP_ANALOGIN_PIN_NUMBER   = 3;
 static const int MAX_NAVX_MXP_ANALOGOUT_PIN_NUMBER  = 1;
